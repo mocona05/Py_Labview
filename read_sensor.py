682109e5e9710a5744896e86746bd94dcc884388
mocona05 <mocona05@gmail.com>
2015-03-30 22:09:43 +0900

    1.05

--- read_sensor.py
+++ read_sensor.py
@@ -2,10 +2,10 @@
 import time
 import ms5611
 import ht_01s
-import cal_o2
-import multiprocessing
-import threading
-import datetime
+#import cal_o2
+#import multiprocessing
+#import threading
+#import datetime
 import sys
 
 ht01s = ht_01s.Ht_01s()
@@ -17,9 +17,9 @@ class Update_sensor:
     def sensor_update(self):
         press, temp1 = MS5611.baro_read()
         humi, temp2, status = ht01s.humi_temp_read()
-        air_oxygen= ari_oxy.cal_oxygen(temp1, humi, press)
-        data = ("Oxy=%2.2f%%, Humi=%4.2f%%, Temp1=%3.2fdegC, Press=%4.2fkPa, Temp2=%3.2fdegC, HT-01s status= %d" 
-          %(air_oxygen, humi, temp1, press, temp2, status))
+ #       air_oxygen= ari_oxy.cal_oxygen(temp1, humi, press)
+        data = ("Humi=%4.2f%%, Temp1=%3.2fdegC, Press=%4.2fkPa, Temp2=%3.2fdegC, HT-01s status= %d" 
+          %(humi, temp1, press, temp2, status))
         return(data)
 
 if __name__ =="__main__":
