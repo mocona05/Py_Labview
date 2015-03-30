abbc4a7275780034dda7006b0af0ddac129df2dc
mocona05 <mocona05@gmail.com>
2015-03-30 23:00:58 +0900

    1.112

--- myShild.py
+++ myShild.py
@@ -1,7 +1,6 @@
 from Tkinter import *
 import time
 import ads7828
-#import adc_read
 import serial
 import pygame
 import wx
@@ -10,26 +9,6 @@ usbport ='/dev/ttyAMA0'
 uart=serial.Serial(usbport,115200)
 pygame.mixer.init()
 
-#i2c-bus adc(ads7828) set 
-#ADS7828_ADDRESS =0x48 #(7bit)
-#I2C_CHANNEL = 1  #raspberry i2c channel
-#adc set
-#PULL_UP_REG =41.2
-#PULL_DOWN_REG =10.0
-#ADC_REF_VOLT =5.0
-#DEFAULT_RATIO =1.0
-
-#temperature sensor set
-#TC4047_ZERO_DEGC =0.5
-#TC4047_DEGC_FOR_VOLT =0.01
-
-#adc = ads7828.ADS7828(ADS7828_ADDRESS, 1, True)
-
-#ADC = adc_read.Voltage_cal(ADS7828_ADDRESS, I2C_CHANNEL, ADC_REF_VOLT,DEFAULT_RATIO,0 )
-#ADC.tempRatio(TC4047_DEGC_FOR_VOLT, TC4047_ZERO_DEGC, 0)
-#ADC.ratioSet(PULL_UP_REG,PULL_DOWN_REG,4)
-#ADC.ratioSet(PULL_UP_REG,PULL_DOWN_REG,5)
-#ADC.ratioSet(PULL_UP_REG,PULL_DOWN_REG,6)
 
 adc_init = ads7828.Ads7828()
 adc = ads7828.Voltage_cal()
@@ -77,7 +56,6 @@ class MyTimer(wx.Timer):
         f.txt.SetLabel('Temprature :' + str(adc.read_temp(0))+' degC') 
         f.txt2.SetLabel('DC Voltage :' +str(adc.read_voltage(6))+' V') 
         f.txt3.SetLabel('BAT Voltage :' +str(adc.read_voltage(7))+' V') 
-#        f.txt3.SetLabel(unicode(sh.Cells(3,1).Value)) 
 
 app = wx.App(0) 
 f = Application(None) 
